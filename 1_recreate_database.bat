796926b3189ed5ce64fc878072dde1edd0aab532
ar-ge <aaron.gergely@yahoo.com>
2016-04-21 23:03:12 +0200

    Update 1_recreate_database.bat

--- 1_recreate_database.bat
+++ 1_recreate_database.bat
@@ -1,10 +1,10 @@
-SET OSMOSIS_SQLDIR="C:/Program Files (x86)/osmosis/script"
 SET PGUSER=postgres
 SET PGPASSWORD=postgres
+
 
 dropdb benin
 createdb benin
 
-psql -U %PGUSER% -d benin -a -f create_extensions.sql
-psql -U %PGUSER% -d benin -a -f %OSMOSIS_SQLDIR%/pgsnapshot_schema_0.6.sql
-psql -U %PGUSER% -d benin -a -f %OSMOSIS_SQLDIR%/pgsnapshot_schema_0.6_linestring.sql
+psql -U %PGUSER% -d benin -a -f SQL/create_extensions.sql
+psql -U %PGUSER% -d benin -a -f SQL/pgsnapshot_schema_0.6.sql
+psql -U %PGUSER% -d benin -a -f SQL/pgsnapshot_schema_0.6_linestring.sql
